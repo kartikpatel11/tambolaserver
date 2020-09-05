fe1115d31caf9812524424a56d2fcdc9fa1c6d15
kartikpatel11 <patel.kartikv@gmail.com>
2020-09-05 18:44:48 +0530

    adding final set of files for schema creation and mongoose changes

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ const express = require('express')
 , path = require('path')
 , mongoose = require('mongoose').set('debug', true)
 , bodyParser = require('body-parser');
+/*, admin = require('firebase-admin')
+
+
+
+var serviceAccount = require("./tambolaonline-afc88-firebase-adminsdk-hgpki-d46c71983e.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tambolaonline-afc88.firebaseio.com"
+});*/
+
+// The topic name can be optionally prefixed with "/topics/".
+var topic = '1';
+
+var message = {
+  data: {
+    number: '74',
+    /*winner: {
+      FIRST_ROW: "Kartik"
+    } */
+  },
+  topic: topic
+};
 
 
 var app = express();
@@ -16,13 +39,21 @@ mongoose.connect('mongodb://localhost/tamboladb',{useNewUrlParser: true});
 
 var api = require("./controllers/api")
 
-app.get('/getmyticket', api.getmyticket);
-app.get('/savemygame', api.savegame);
-app.get('/getmyactivegames',api.getmyactivegames)
+app.get('/getmyticket', api.getmyticket); //Called by participant of a game
+app.get('/savemygame', api.savegame); // Called by host of the game to save his/her game
+app.get('/getmyactivegames',api.getmyactivegames) //Called by everyone to get all active games
+app.post('/storegamestatus',api.storegamestatus) //Called during game to update game status
+app.get('/getmyhostedgame',api.getmyhostedgame) //Called by host of the game to start the game 
 app.get('/', function(req,res){
-    res.send("Hello world to "+req.query.myname);
+  res.send("Hello world to "+req.query.myname);
+
 });
 
+
+
+
+
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 80!');
 });
--- controllers/api.js
+++ controllers/api.js
@@ -1,5 +1,16 @@
 var mongoose=require('mongoose'),
- game= require("../models/game")
+ game= require("../models/game"),
+admin = require('firebase-admin'),
+ bodyParser = require('body-parser');
+
+ var serviceAccount = require("../tambolaonline-afc88-firebase-adminsdk-hgpki-d46c71983e.json");
+const { hasUncaughtExceptionCaptureCallback } = require('process');
+
+ admin.initializeApp({
+   credential: admin.credential.cert(serviceAccount),
+   databaseURL: "https://tambolaonline-afc88.firebaseio.com"
+ });
+ 
 
 exports.savegame= function(req,res){
 var game = new Game(req.body);
@@ -19,9 +30,45 @@ game.save(function (err,log) {
     });
 };
 
+exports.storegamestatus = function(req, res) {
+    console.log("In storegamestatus  GameID:"+req.body.gid+",currnum="+req.body.currnum);
+
+    game.findOneAndUpdate({gameid:req.body.gid},{$push: {currentState:req.body.currnum}},function(err,doc){
+        if(err)
+        {
+            console.log('Error updating Game:'+req.body.gid+" with current number: "+req.body.currnum);
+            res,status(300).send({errmsg: "not able to update game status"});
+        }
+        else
+        {
+            //send number details to FCM for players to know
+            var message = {
+                data: {
+                  number: req.body.currnum,
+                  /*winner: {
+                    FIRST_ROW: "Kartik"
+                  } */
+                },
+                topic: req.body.gid
+              };
+
+              admin.messaging().send(message)
+              .then((response) => {
+                  // Response is a message ID string.
+                  console.log('Successfully sent message:', response);
+                  res.status(200).json("Message sent Successfully");
+                })
+                .catch((error) => {
+                  console.log('Error sending message:', error);
+                });   
+
+        }
+    });
+
+};
 
 exports.getmyticket = function(req, res){
-    console.log("In getmycard activity GameID:"+req.query.gid+",ParticipantID="+req.query.pid);
+    console.log("In getmycard activity GameID:"+req.query.gid+",ParticipantID="+req.query.phno);
 
    game.aggregate(
        [
@@ -43,7 +90,7 @@ exports.getmyticket = function(req, res){
                         {
                             input: '$participants',
                             as: 'participant',
-                            cond: {"$eq" : ['$$participant.participantid',req.query.pid]}
+                            cond: {"$eq" : ['$$participant.phone',req.query.phno]}
 
                         }
                    }
@@ -85,4 +132,12 @@ exports.getmyactivegames = function(req,res) {
         } 
     );
 };
+
+exports.getmyhostedgame = function(req,res) {
+    console.log("In getmyhostedgame gid:"+req.query.gid);
+
+    game.findOne({gameid:req.query.gid}, function(err,docs){
+        res.json(200,docs)
+    }
+)};
 
